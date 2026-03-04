f0cbd691bfef4906c05ff862d2de386c7064d5cd
jay <phannindrajaya12@gmail.com>
2026-03-05 02:35:07 +0530

    Export added to api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://unverbose-dessie-glacial.ngrok-free.dev"; //need to change the route whenever we create a new backedn link using Ngrok
+export const API_BASE  = "https://unverbose-dessie-glacial.ngrok-free.dev"; //need to change the route whenever we create a new backedn link using Ngrok
 
 export async function sendMessage(message) {
   const response = await fetch(`${API_BASE}/chat`, {
